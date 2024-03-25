aa658bf3aa423a60fcf773a5b5c7eeac8d427958
Meharjeet Singh <meharjeet_singh@sfu.ca>
2024-03-25 00:08:54 -0700

    ui-changes and connected allposts to the backend

--- src/pages/post/AllPosts.jsx
+++ src/pages/post/AllPosts.jsx
@@ -1,48 +1,19 @@
-// import { usePosts } from "../../hooks";
+import { usePosts } from "../../hooks";
 
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import FormHeading from "@/components/FormHeading";
 import { Card, CardTitle, CardDescription } from "@/components/ui/card";
 import { Link } from "react-router-dom";
 
 function AllPostsPage() {
-	// const { posts } = usePosts();
-	const posts = useMemo(
-		() => [
-			{
-				postedBy: "696969696969",
-				createdAt: {
-					_seconds: 1710123553,
-					_nanoseconds: 994000000,
-				},
-				image: "abcde.jpeg",
-				comments: [],
-				description:
-					"This is a really good course. Based on Computing Systems.",
-				postId: "41e3632f-1da8-44cc-b961-22b88ee96be7",
-			},
-			{
-				postedBy: "44444444",
-				createdAt: {
-					_seconds: 1710123690,
-					_nanoseconds: 377000000,
-				},
-				image: "xyiugfnoufnheriu.jpeg",
-				comments: [],
-				postId: "54c9bb64-d238-4ce6-8404-2b37cb2ef3db",
-				description:
-					"This is a really bad course. Based on Software Systems.",
-			},
-		],
-		[]
-	);
+	const { posts } = usePosts();
 
 	const [searchTerm, setSearchTerm] = useState("");
-	const [searchResults, setSearchResults] = useState(posts);
+	const [searchResults, setSearchResults] = useState([posts]);
 
 	useEffect(() => {
-		const results = posts.filter(
+		const results = posts?.filter(
 			(post) =>
 				post?.postedBy?.toLowerCase().includes(searchTerm) ||
 				post?.description?.toLowerCase().includes(searchTerm)
@@ -65,8 +36,10 @@ function AllPostsPage() {
 							No posts...
 						</span>
 					)}
-					{searchResults.map((post) => (
-						<PostPreview post={post} key={post.postId} />
+					{searchResults.map((post, index) => (
+						<div key={index}>
+							<PostPreview post={post} key={index} />
+						</div>
 					))}
 				</div>
 			</div>

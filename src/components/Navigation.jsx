3192b6c749b157a06d5c8be6f9a158e6e63b6d17
Meharjeet Singh <meharjeet_singh@sfu.ca>
2024-03-25 00:06:50 -0700

    ui-changes + signout

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,16 +7,15 @@ import { Button } from "@/components/ui/button";
 
 import { useAuth } from "@/hooks";
 import { ModeToggle } from "./Mode-toggle";
+import {
+	HoverCard,
+	HoverCardContent,
+	HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { LucideChevronDown, LucideSettings, LogOut } from "lucide-react";
 
 function Navigation() {
-	const { isLoggedIn } = useAuth();
-
-	if (!isLoggedIn) {
-		console.log(isLoggedIn);
-		console.log("User is not logged in");
-	}
-
-	// console.log("isLoggedIn in navigation", isLoggedIn);
+	const { isLoggedIn, signout } = useAuth();
 
 	return (
 		<nav className="flex w-full p-2 justify-between items-center">
@@ -33,9 +32,49 @@ function Navigation() {
 						<Button asChild variant="ghost">
 							<NavLink to="/posts">Lectures</NavLink>
 						</Button>
-						<Button asChild variant="ghost">
-							<NavLink to="/profile">Profile</NavLink>
-						</Button>
+						<HoverCard openDelay={250}>
+							<HoverCardTrigger asChild>
+								<Button
+									asChild
+									variant="ghost"
+									className="flex"
+								>
+									<NavLink
+										to="/profile"
+										className="group flex gap-2 items-center justify-center"
+									>
+										<span>Profile</span>
+										<LucideChevronDown className="size-4 group-hover:rotate-180 transition-transform duration-300 ease-in-out flex items-center gap-1" />
+									</NavLink>
+								</Button>
+							</HoverCardTrigger>
+							<HoverCardContent className="flex w-full gap-2 p-2 flex-col items-center justify-center duration-300">
+								<Button
+									asChild
+									variant="ghost"
+									size="sm"
+									className="w-full group"
+								>
+									<slot className="w-full flex justify-between items-center">
+										<LucideSettings className="size-4 mr-4 group-hover:rotate-45 duration-300" />
+										<NavLink to="/settings">
+											Settings
+										</NavLink>
+									</slot>
+								</Button>
+								<Button
+									variant="destructive"
+									className="w-full"
+									size="sm"
+									onClick={() => signout()}
+								>
+									<slot className="w-full flex justify-between items-center">
+										<LogOut className="size-4 mr-4" />
+										<span>Sign Out</span>
+									</slot>
+								</Button>
+							</HoverCardContent>
+						</HoverCard>
 					</>
 				) : (
 					<>
@@ -58,3 +97,4 @@ function Navigation() {
 }
 
 export default Navigation;
+//

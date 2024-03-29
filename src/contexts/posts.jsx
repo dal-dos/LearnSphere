c2fd0a1ee0c3d5895ccf440948862d0155e9a996
Arjun <arjun.shokar@gmail.com>
2024-03-28 23:45:19 -0700

    forgot to stage all posts and posts.jsx

--- src/contexts/posts.jsx
+++ src/contexts/posts.jsx
@@ -1,58 +1,170 @@
 import { useState, createContext, useEffect } from "react";
-
+import { useAuth } from "@/hooks";
 import { POSTS_BASE_URL } from "../constants";
 
-import { useAuth } from "@/hooks";
-
 export const PostsContext = createContext({
-	posts: [],
+  posts: [],
+  handleAddComment: () => {},
+  handleDeletePost: () => {},
+  handleDeleteComment: () => {},
+  handleCreatePost: () => {},
+  handleUpdatePost: () => {}
 });
+
 export default function PostsProvider({ children }) {
-	const { isLoggedIn, getToken } = useAuth();
+  const { isLoggedIn, getToken } = useAuth();
 
-	const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  
 
-	useEffect(() => {
-		if (!isLoggedIn) {
-			return;
-		}
-		async function fetchPosts() {
-			setPosts(await getPosts(`token=${getToken()}`));
-		}
-		fetchPosts();
-	}, [isLoggedIn, getToken]);
+  useEffect(() => {
+    if (!isLoggedIn) {
+      return;
+    }
+    async function fetchPosts() {
+      setPosts(await getPosts(`token=${getToken()}`));
+    }
+    fetchPosts();
+  }, [isLoggedIn, getToken]);
 
-	return (
-		<PostsContext.Provider value={{ posts }}>
-			{children}
-		</PostsContext.Provider>
-	);
-}
+  const getPosts = async (token) => {
+    try {
+      const response = await fetch(`${POSTS_BASE_URL}/posts`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": POSTS_BASE_URL,
+        },
+      });
 
-async function getPosts(token) { //gets all posts
+      if (!response.ok) {
+        console.error("Failed to fetch posts");
+        return [];
+      }
+
+      const data = await response.json();
+      return data.post;
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      return [];
+    }
+  };
+
+  const handleAddComment = async (postId, comment) => {
+    try {
+      const response = await fetch(`${POSTS_BASE_URL}/posts/${postId}/addcomment`, {
+        method: 'POST',
+        headers: {
+			'Authorization': `Bearer token=${getToken()}`,
+          "Content-Type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": POSTS_BASE_URL,
+        },
+        body: JSON.stringify({
+          comment,
+        }),
+      });
+      const data = await response.json();
+      console.log('Add Comment Response:', data);
+      // Update posts state if necessary
+    } catch (error) {
+      console.error('Error adding comment:', error);
+    }
+  };
+
+  const handleDeletePost = async (postId) => {
+    try {
+		
+      const response = await fetch(`${POSTS_BASE_URL}/posts/delete/${postId}`, {
+        method: 'DELETE',
+        headers: {
+			'Authorization': `Bearer token=${getToken()}`,
+          "Content-Type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": POSTS_BASE_URL,
+        },
+      });
+	  console.log(getToken());
+      const data = await response.json();
+      console.log('Delete Post Response:', data);
+	  setPosts(posts.filter(post => post.postId !== postId));
+    } catch (error) {
+      console.error('Error deleting post:', error);
+    }
+  };
+
+  const handleDeleteComment = async (postId, commentId) => {
+    try {
+      const response = await fetch(`${POSTS_BASE_URL}/posts/${postId}/comments/${commentId}/delete`, {
+        method: 'DELETE',
+        headers: {
+			'Authorization': `Bearer token=${getToken()}`,
+          "Content-Type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": POSTS_BASE_URL,
+        },
+      });
+      const data = await response.json();
+      console.log('Delete Comment Response:', data);
+      // Update posts state if necessary
+    } catch (error) {
+      console.error('Error deleting comment:', error);
+    }
+  };
+
+
+
+const handleCreatePost = async ({ userId, title, description, image, lectureURL }) => {
 	try {
-		const response = await fetch(`${POSTS_BASE_URL}/posts`, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-				"Content-Type": "application/json;charset=UTF-8",
-				"Access-Control-Allow-Origin": POSTS_BASE_URL,
-			},
-		});
-
-		if (!response) {
-			console.log("No Response: While fetching posts");
-			return [];
-		}
-		const data = await response.json();
-
-		if (!data || data.success === false) {
-			console.log("getPosts() failed: ", data.message);
-			return [];
-		}
-		console.log(data.post);
-		return data.post;
+	  const response = await fetch(`${POSTS_BASE_URL}/posts/create`, {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer token=${getToken()}`,
+		  'Content-Type': 'application/json;charset=UTF-8',
+		  'Access-Control-Allow-Origin': POSTS_BASE_URL,
+		},
+		body: JSON.stringify({
+			userId, 
+			title,
+			description,
+			image,
+			lectureURL,
+		  }),
+	  });
+	  const data = await response.json();
+	  console.log('Create Post Response:', data);
+	  
 	} catch (error) {
-		console.log(error);
-		return [];
+	  console.error('Error creating post:', error);
 	}
+  };
+  
+
+  
+const handleUpdatePost = async (postId, { title, description, image, lectureURL }) => {
+	try {
+	  const response = await fetch(`${POSTS_BASE_URL}/posts/update/${postId}`, {
+		method: 'PUT',
+		headers: {
+			'Authorization': `Bearer token=${getToken()}`,
+		  'Content-Type': 'application/json;charset=UTF-8',
+		  'Access-Control-Allow-Origin': POSTS_BASE_URL,
+		},
+		body: JSON.stringify({
+		  title,
+		  description,
+		  image,
+		  lectureURL,
+		}),
+	  });
+	  const data = await response.json();
+	  console.log(data);
+	  console.log('Update Post Response:', data);
+	} catch (error) {
+	  console.error('Error updating post:', error);
+	}
+  };
+
+  return (
+    <PostsContext.Provider value={{ posts, handleAddComment, handleDeletePost, handleDeleteComment, handleCreatePost, handleUpdatePost }}>
+      {children}
+    </PostsContext.Provider>
+  );
 }
--- src/pages/post/AllPosts.jsx
+++ src/pages/post/AllPosts.jsx
@@ -1,71 +1,88 @@
-import { usePosts } from "../../hooks";
-
+import { usePosts } from "@/hooks";
 import { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import FormHeading from "@/components/FormHeading";
-import { Card, CardTitle, CardDescription, CardContent, CardHeader, CardFooter} from "@/components/ui/card";
+import { Card, CardTitle, CardDescription } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { useProfile } from "@/hooks";
+import { useNavigate } from 'react-router-dom';
 
 function AllPostsPage() {
-    const { posts } = usePosts();
+	
+  	const { posts, handleCreatePost } = usePosts(); 
 
-    const [searchTerm, setSearchTerm] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
+	const [searchResults, setSearchResults] = useState([posts]);
+	const { profile } = useProfile();
 
-    useEffect(() => {
-        const results = posts.filter(
-            (post) =>
-                post.postedBy.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                post.description.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        setSearchResults(results);
-    }, [searchTerm, posts]);
+	const navigate = useNavigate();
 
-    return (
-        <>
-            <FormHeading>All Lectures</FormHeading>
-            <div className="w-[90%] mx-auto">
-                <Input
-                    placeholder="Search Posts"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <div className="flex flex-col gap-2 justify-center items-center mt-2">
-                    {searchResults.length === 0 && (
-                        <span className="text-center text-xl mt-10">No posts...</span>
-                    )}
-                    {searchResults.map((post, index) => (
-                        <div key={index} className="w-full">
-                            <PostPreview post={post} />
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </>
-    );
+	useEffect(() => {
+		const results = posts?.filter(
+			(post) =>
+				post?.postedBy?.toLowerCase().includes(searchTerm) ||
+				post?.description?.toLowerCase().includes(searchTerm)
+		);
+		setSearchResults(results);
+	}, [searchTerm, posts]);
+
+	const addPost = () => {
+		navigate('/posts/add');
+	};
+	  
+
+	  return (
+		<>
+			<div className="min-h-screen">
+				<div className="max-w-4xl mx-auto py-8 px-4">
+					<FormHeading className="text-3xl font-bold text-center text-blue-800 mb-8">All Lectures</FormHeading>
+					
+					<div className="flex justify-center mb-6">
+						<Button onClick={addPost} className="bg-blue-600 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition ease-in-out duration-150">
+							Create Post
+						</Button>
+					</div>
+					
+					<div className="mb-6">
+						<Input
+							placeholder="Search Posts"
+							value={searchTerm}
+							onChange={(e) => setSearchTerm(e.target.value)}
+							className="w-full p-4 rounded border-2 border-blue-300 focus:border-blue-500 transition ease-in-out duration-150"
+						/>
+					</div>
+					
+					<div className="space-y-4">
+						{searchResults.length === 0 ? (
+							<span className="text-center text-xl mt-10 block text-blue-800">
+								No posts found...
+							</span>
+						) : (
+							searchResults.map((post, index) => (
+								<div key={index} className="bg-blue-50 rounded-lg shadow p-2">
+									<PostPreview post={post} />
+								</div>
+							))
+						)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+	
+	
 }
 
 function PostPreview({ post }) {
-    return (
-        <Card className="w-full p-4 hover:bg-muted cursor-pointer">
-            <Link to={`/posts/${post.postId}`} className="w-full" activeClassName="current">
-                <CardHeader>
-                    <CardTitle>{post.title}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <CardDescription>{post.description}</CardDescription>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    <div>
-                        <CardDescription>Teacher: {post.postedBy}</CardDescription>
-                    </div>
-                    <div>
-                        <CardDescription className="text-sm text-gray-500">{new Date(post.createdAt._seconds * 1000).toLocaleString()}</CardDescription>
-                    </div>
-                </CardFooter>
-            </Link>
-        </Card>
-    );
+	return (
+		<Card className="w-full p-4 hover:bg-muted cursor-pointer">
+			<Link to={`/posts/${post.postId}`} activeclassname="current">
+				<CardTitle>{post.postedBy}</CardTitle>
+				<CardDescription>{post.description}</CardDescription>
+			</Link>
+			
+		</Card>
+	);
 }
 export default AllPostsPage;
-

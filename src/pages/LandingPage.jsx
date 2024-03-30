d0aac3a4e0bdcf7ca5d89e88ca902e113d12b2a8
Meharjeet Singh <meharjeet_singh@sfu.ca>
2024-03-29 23:20:49 -0700

    ui changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,25 +1,19 @@
-// Home.tsx
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import "@/global.css";
 
 function Home() {
 	return (
-		<>
-			<section className="h-[100dvh] flex items-start justify-center">
-				<div className="pt-32 text-center space-y-8">
-				<img src="./assets/learnspherelogo-text.png" alt="LearnSphere Logo" className="w-64 h-auto mx-auto mb-8" />
-					<h1 className="text-5xl font-bold tracking-tight">
-						Welcome
-					</h1>
-
-					<Button className="hero-cta" asChild>
-						<Link to="/dashboard">Explore Courses</Link>
-					</Button>
-				</div>
-			</section>
-		</>
+		<section className="h-full w-full flex flex-col gap-16 items-center justify-center">
+			<img
+				src="./assets/learnspherelogo-text.png"
+				alt="LearnSphere Logo"
+				className="w-56"
+			/>
+			<Button asChild>
+				<Link to="/dashboard">Explore Courses</Link>
+			</Button>
+		</section>
 	);
 }
 

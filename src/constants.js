2ab8ddfd5d25a1fbeb5e86b10f59746bab4ecb6d
Meharjeet Singh <meharjeet_singh@sfu.ca>
2024-07-06 22:10:16 -0700

    merge

--- src/constants.js
+++ src/constants.js
@@ -1,22 +1,26 @@
 // TODO: UPDATE THE MICROSERVICE URLS HERE ( WITHOUT THE TRAILING SLASH )
 
-export const POSTS_BASE_URL = "http://localhost:8080/api/post-service";
-export const AUTH_BASE_URL = "http://localhost:8090/api/auth-service";
-export const PROFILE_BASE_URL = "http://localhost:8100/api/profile-service";
+// export const POSTS_BASE_URL = "http://localhost:8080/api/post-service";
+// export const AUTH_BASE_URL = "http://localhost:8090/api/auth-service";
+// export const PROFILE_BASE_URL = "http://localhost:8100/api/profile-service";
+
+export const PROFILE_BASE_URL =
+	"https://gateway-7-2bvt8rd0.wl.gateway.dev/api/profile-service";
+export const AUTH_BASE_URL =
+	"https://gateway-7-2bvt8rd0.wl.gateway.dev/api/auth-service";
+export const POSTS_BASE_URL =
+	"https://gateway-7-2bvt8rd0.wl.gateway.dev/api/post-service";
 
 // export const PROFILE_BASE_URL =
-// "https://profile-service-dot-learn-sphere-3.uc.r.appspot.com/api/profile-service";
+// 	"https://trail-api-1ptj1n1y.uc.gateway.dev/api/profile-service";
 // export const AUTH_BASE_URL =
-// "https://auth-service-dot-learn-sphere-3.uc.r.appspot.com/api/auth-service";
+// 	"https://trail-api-1ptj1n1y.uc.gateway.dev/api/auth-service";
 // export const POSTS_BASE_URL =
-// "https://post-service-dot-learn-sphere-3.uc.r.appspot.com/api/post-service";
+// 	"https://trail-api-1ptj1n1y.uc.gateway.dev/api/post-service";
 
 export const CORS_CONFIG = {
 	"Content-Type": "application/json;charset=UTF-8",
 	"Access-Control-Allow-Origin": "*",
-};
-export const PROFILE_CORS_CONFIG = {
-	"Content-Type": "application/json;charset=UTF-8",
-	"Access-Control-Allow-Origin": "http://localhost:8100",
-	"Access-Control-Allow-Credentials": "true",
+	"Access-Control-Allow-Methods": "*",
+	"Access-Control-Allow-Headers": "*",
 };
